c946e40cc32abad637e970936c8fe811a2ae2c81
LukasMurdock <lukas.murdock@gmail.com>
2023-04-20 22:31:03 -0400

    fix getAll unknown types

--- lib/core.d.ts
+++ lib/core.d.ts
@@ -1,7 +1,10 @@
 /// <reference types="node" />
-declare type Config = Record<string, (val: string | null) => string | number | unknown>;
-declare type ArrayConfig = Record<string, (val: string[]) => string | number | unknown>;
-export declare function simpleParams<GetConfig extends Config, GetAllConfig extends ArrayConfig, GetConfigKey extends keyof GetConfig, GetAllConfigKey extends keyof GetAllConfig>(getConfig: GetConfig, getAllConfig?: GetAllConfig): {
-    parse(params: URLSearchParams): { [K in GetConfigKey]: ReturnType<GetConfig[K]>; } & { [K_1 in GetAllConfigKey]: ReturnType<GetAllConfig[K_1]>; };
+declare type ParamsConfig<Parsed> = Record<string, (val: Parsed) => string | number | unknown>;
+declare type Config = ParamsConfig<ReturnType<URLSearchParams['get']>>;
+declare type ArrayConfig = ParamsConfig<ReturnType<URLSearchParams['getAll']>>;
+export declare function simpleParams<T1 extends Config, T2 extends ArrayConfig | undefined, R1 extends ReturnType<typeof get<T1, string>>, FR extends T2 extends undefined ? R1 : R1 & ReturnType<typeof getAll<T2 & {}, string>>>(getConfig: T1, getAllConfig?: T2): {
+    parse(params: URLSearchParams): FR;
 };
+declare function get<T extends Config, K extends keyof Config>(config: T, params: URLSearchParams): { [K_1 in keyof T]: ReturnType<T[K_1]>; };
+declare function getAll<T extends ArrayConfig, K extends keyof ArrayConfig>(config: T, params: URLSearchParams): { [K_1 in keyof T]: ReturnType<T[K_1]>; };
 export {};
--- src/core.ts
+++ src/core.ts
@@ -1,37 +1,55 @@
-type Config = Record<string, (val: string | null) => string | number | unknown>;
-type ArrayConfig = Record<string, (val: string[]) => string | number | unknown>;
+type ParamsConfig<Parsed> = Record<
+	string,
+	(val: Parsed) => string | number | unknown
+>;
+
+type Config = ParamsConfig<ReturnType<URLSearchParams['get']>>;
+type ArrayConfig = ParamsConfig<ReturnType<URLSearchParams['getAll']>>;
+
+// Naming scheme:
+// T1: Type 1
+// T2: Type 2
+// R1: Return 1
+// FR: Final Return
 
 export function simpleParams<
-	GetConfig extends Config,
-	GetAllConfig extends ArrayConfig,
-	GetConfigKey extends keyof GetConfig,
-	GetAllConfigKey extends keyof GetAllConfig
->(getConfig: GetConfig, getAllConfig?: GetAllConfig) {
+	T1 extends Config,
+	T2 extends ArrayConfig | undefined,
+	R1 extends ReturnType<typeof get<T1, string>>,
+	FR extends T2 extends undefined
+		? R1
+		: R1 & ReturnType<typeof getAll<T2 & {}, string>>
+>(getConfig: T1, getAllConfig?: T2) {
 	return {
-		parse(params: URLSearchParams) {
-			const getParams = Object.entries(getConfig).reduce(
-				(acc, [key, parseFunction]) => {
-					acc[key as GetConfigKey] = parseFunction(
-						params.get(key)
-					) as ReturnType<GetConfig[GetConfigKey]>;
-					return acc;
-				},
-				{} as { [K in GetConfigKey]: ReturnType<GetConfig[K]> }
-			);
-
-			const getAllParams = getAllConfig
-				? Object.entries(getAllConfig).reduce((acc, [key, parseFunction]) => {
-						acc[key as GetAllConfigKey] = parseFunction(
-							params.getAll(key)
-						) as ReturnType<GetAllConfig[GetAllConfigKey]>;
-						return acc;
-				  }, {} as { [K in GetAllConfigKey]: ReturnType<GetAllConfig[K]> })
-				: ({} as { [K in GetAllConfigKey]: ReturnType<GetAllConfig[K]> });
-
-			return {
-				...getParams,
-				...getAllParams,
-			};
+		parse(params: URLSearchParams): FR {
+			if (!getAllConfig) {
+				return get(getConfig, params) as FR;
+			} else {
+				return {
+					...get(getConfig, params),
+					...getAll(getAllConfig, params),
+				} as FR;
+			}
 		},
 	};
+}
+
+function get<T extends Config, K extends keyof Config>(
+	config: T,
+	params: URLSearchParams
+) {
+	return Object.entries(config).reduce((acc, [key, parseFunction]) => {
+		acc[key as keyof T] = parseFunction(params.get(key)) as ReturnType<T[K]>;
+		return acc;
+	}, {} as { [K in keyof T]: ReturnType<T[K]> });
+}
+
+function getAll<T extends ArrayConfig, K extends keyof ArrayConfig>(
+	config: T,
+	params: URLSearchParams
+) {
+	return Object.entries(config).reduce((acc, [key, parseFunction]) => {
+		acc[key as keyof T] = parseFunction(params.getAll(key)) as ReturnType<T[K]>;
+		return acc;
+	}, {} as { [K in keyof T]: ReturnType<T[K]> });
 }
